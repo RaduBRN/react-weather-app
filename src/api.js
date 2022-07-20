387f381d97dc1713ad26dcce7329a91b6f903578
Radu Baroncea <baroncea.radu@gmail.com>
2022-07-20 09:17:41 +0300

    add geolocation api to search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1b3cc69528msha34d72539ab7ca1p12b964jsna7e8980158e7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
